4dfa29b38e239215aa217389dc6d466d08f0828f
Salil <salilgokhale24@gmail.com>
2022-03-25 15:21:08 +0530

    Completed Wordle+

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,14 +1,28 @@
 const http = require("http");
 
-const SECRET = "CIGAR"; // You can set any word as the secret answer
+const SECRET = "NUKES"; // You can set any word as the secret answer
 
-function myFunction(req, res) {
-	// console.log({req}); // You can uncomment this to see the request object
-	console.log(req.url);
-
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
+function myFunction(req, res)
+{
+	//console.log({ req }); // You can uncomment this to see the request object
+	const request_url = req.url;
+	const GUESS = String(request_url).substring(request_url.indexOf('=') + 1); // Write logic to parse the word which the user guessed from the URL string
+	let feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	for (let i = 0; i < 5; i++)
+	{
+		if (GUESS[i] == SECRET[i])
+		{
+			feedback += "g";
+		}
+		else if (SECRET.includes(GUESS[i]))
+		{
+			feedback += "y";
+		}
+		else
+		{
+			feedback += "b";
+		}
+	}
 	res.write(feedback);
 	res.end();
 }
